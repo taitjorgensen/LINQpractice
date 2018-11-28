bcf2844dc8ae15b1e4dc5f1c478b69c70aa05830
taitjorgensen <taitjorgensen@gmail.com>
2018-11-27 19:49:48 -0600

    Completed the string letters! Final git push.

--- LINQProblems/Program.cs
+++ LINQProblems/Program.cs
@@ -22,7 +22,43 @@ namespace LINQProblems
                 Console.ReadLine();
             }
 
+            var distinctNames = names.Select(n => n).Distinct();
+            foreach (var name in distinctNames)
+            {
+                Console.WriteLine(name);
+                Console.ReadLine();
+            }
 
+            var listOfList = classGrades.Select(c => c.Split(',')).ToList();
+            List<double> averages = new List<double>();
+            double totalAverage;
+
+            foreach (var item in listOfList)
+            {
+                List<int> studentGrades = new List<int>();
+                foreach (var thing in item)
+                {
+                    studentGrades.Add(int.Parse(thing));
+                }
+                studentGrades.OrderByDescending(s => s);
+                studentGrades.RemoveAt(studentGrades.Count - 1);
+                double studentAverage = studentGrades.Average();
+                averages.Add(studentAverage);
+            }
+            totalAverage = averages.Average();
+            Console.WriteLine(totalAverage);
+            Console.ReadLine();
+
+            //String Letters
+            string stringEntered = "Terrill";
+            var letterRepeat = from l in stringEntered.ToLower().ToArray().OrderBy(l => l)
+                               group l by l into letterFrequency
+                               select letterFrequency;
+
+            foreach (var l in letterRepeat)
+            Console.Write($"{l.Key}{l.Count()}");
+            Console.ReadLine();
         }
+    
     }
 }
